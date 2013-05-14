3bf24177510e9cde9766f91d1b1c1a02c5c3c618
Casey Patrizio <cpatrizi@swift.eos.ubc.ca>
2013-05-14 15:12:52 -0700

    fixed netcdf4 format error

--- netcdf/initialize_ncFile.py
+++ netcdf/initialize_ncFile.py
@@ -1,26 +1,34 @@
 from netCDF4 import Dataset
 import numpy as np
+import os
 
 #initializes a netCDF file to hold a single variable with dimensions x, y, z, time
 #dimension length is specified by user
 def initialize_ncFile(filename, varname, xlen, ylen, zlen, tlen): 
+    
 
-	nc_out = Dataset(filename, mode='w', format='NETCDF3_CLASSIC')
-	nc_out.createDimension('x', xlen)
-	nc_out.createDimension('y', ylen)
-	nc_out.createDimension('z', zlen)
-	nc_out.createDimension('time', tlen)
+    #an error is thrown if the file already exists, delete it first if it exists
+    try:
+        filepath = os.path.realpath(filename)
+        os.remove(filepath)
+        nc_out = Dataset(filename, mode='w', format='NETCDF4')
+    except OSError:
+        nc_out = Dataset(filename, mode='w', format='NETCDF4')
+    
+    nc_out.createDimension('x', xlen)
+    nc_out.createDimension('y', ylen)
+    nc_out.createDimension('z', zlen)
+    nc_out.createDimension('time', tlen)
+    
+    x = nc_out.createVariable('x', 'f8', ('x',))
+    y = nc_out.createVariable('y', 'f8', ('y',))
+    z = nc_out.createVariable('z', 'f8', ('z',))
+    time = nc_out.createVariable('time', 'f8', ('time',))
+    val = nc_out.createVariable(varname, 'f8', ('time', 'z', 'y', 'x'))
 
-	x = nc_out.createVariable('x', 'f4', ('x',))
-	y = nc_out.createVariable('y', 'f4', ('y',))
-	z = nc_out.createVariable('z', 'f4', ('z',))
-	time = nc_out.createVariable('time', 'f4', ('time',))
-
-	val = nc_out.createVariable(varname, 'f4', ('time', 'z', 'y', 'x'))
-
-	x.units = 'meters'
-	y.units = 'meters'
-	z.units = 'meters'
-	time.units = 'seconds since 2013-05-08 00:00:00 +0:00'
-
-	return nc_out
+    x.units = 'meters'
+    y.units = 'meters'
+    z.units = 'meters'
+    time.units = 'seconds since 2013-05-08 00:00:00 +0:00'
+   
+    return nc_out
--- netcdf/stack_ncFiles.py
+++ netcdf/stack_ncFiles.py
@@ -9,43 +9,41 @@ import glob
 #assuming the variable of interest has 3D spatial dependence
 
 #get a list of all the netcdf BOMEX files in the current directory
-
 ncFile_names = glob.glob('BOMEX*nc')
 print "files to stack: "
 for f in ncFile_names:
 	print f
 #use the first file to set up the dimensions of the output file
 nc_in = Dataset(ncFile_names[0], 'r')
-
 xlen = len(nc_in.variables['x'])
 ylen = len(nc_in.variables['y'])
 zlen = len(nc_in.variables['z'])
 #assume each BOMEX file corresponds to a single time step
 tlen = len(ncFile_names)
-
 print "x length", xlen
 print "y length", ylen
 print "z length", zlen
 print "t length", tlen
-
 #variable to stack
 varname = 'QN'
-outfile = 'testBOMEXfile.nc'
-
+outfile = 'testBOMEXstack.nc'
 print 'creating new file: ', outfile
 nc_out = initialize_ncFile(outfile, varname, xlen, ylen, zlen, tlen)
-nc_out.variables['x'][:] = nc_in.variables['x'][:]
-nc_out.variables['y'][:] = nc_in.variables['y'][:]
-nc_out.variables['z'][:] = nc_in.variables['z'][:]
-
+x = nc_out.variables['x']
+y = nc_out.variables['y']
+z = nc_out.variables['z']
+x[:] = nc_in.variables['x'][:]
+y[:] = nc_in.variables['y'][:]
+z[:] = nc_in.variables['z'][:]
 time = nc_out.variables['time']
 #assume some value for the time step
 dt = 1
 tstart = 0
 tend = tstart + dt*tlen
 time[:] = np.arange(tstart, tend, dt)
-
+time.units = 'seconds since 2013-05-08 00:00:00 +0:00'
 print 'file created, start stacking'
+
 #loop through all of the files
 #at each iteration, fill in the value of the variable of interest at the current 
 #time step
