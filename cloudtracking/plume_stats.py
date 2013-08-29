6a7f7289e360fd6473d05c3bb3006ffbec7355b5
Casey Patrizio <cpatrizio88@gmail.com>
2013-08-29 14:45:02 -0700

    use plume id filtering function

--- cloudtracking/plume_stats.py
+++ cloudtracking/plume_stats.py
@@ -5,7 +5,7 @@ import cloudtracker
 import cPickle
 import glob
 from netCDF4 import Dataset
-from util_functions import index_to_xy
+from util_functions import index_to_xy, filter_clusters
 
 
 """
@@ -20,73 +20,54 @@ from util_functions import index_to_xy
            note: all plumes that exist for only a single model output time step are ignored 
 """
 
-def plume_stats(filenames):
+def plume_stats(filenames, filtered_ids, maxid, MC):
 
     filenames.sort()
-    cluster = cPickle.load(open(filenames[0], 'rb'))[0]
-    MC = cluster.MC
-
-    #get grid parameters
-    dt = MC['dt']/60. #timestep in min
-    nx = MC['nx'] #number of grid points in x, y, and z direction
-    ny = MC['ny']
-    nz = MC['nz']
-    nt = MC['nt'] #number of time steps
-    dx = MC['dx'] #grid spacing (m)
-    dy = MC['dy']
-    dz = MC['dz']
-    #a list of dictionaries; each dictionary contains the set of all
-    #cluster objects at a given time
-    clusters_list = []
-    maxid = 0
-    
-    for fname in filenames:
-        clusters = cPickle.load(open(fname, 'rb'))
-        clusters_list.append(clusters)
-        ids = clusters.keys()
-        maxid_tmp = max(ids)
-        if maxid_tmp > maxid:
-            maxid = maxid_tmp
 
     #lifetimes of clusters
     lifetimes = np.zeros(maxid+1)
     #lifetimes of clusters before condensing
-    lt_before_condensed = np.zeros(maxid+1)
+    lifetimes_moist = np.zeros(maxid+1)
     
     #areas of xy projections of plumes (before condensed region forms)
     areas = np.zeros(maxid+1)
     #keep track of plumes that form condensed regions
     #initialize all plumes to False
     has_condensed = np.zeros(maxid+1) < -1
+
     
-    #clusters is a dictionary of Cluster objects
-    #(see cloud_objects.py in cloudtracker)
-    for t, clusters in enumerate(clusters_list):
+    for fname in filenames:
+        clusters = cPickle.load(open(fname, 'rb'))
         #iterate over each cluster at the current time
         #increment the lifetime of the plume
         #and find the area of plume x-y projection
         for id, cluster in clusters.iteritems():
-           lifetimes[id]  = lifetimes[id] + dt
-           if not has_condensed[id]:
-             lt_before_condensed[id] = lt_before_condensed[id] + dt
-             proj = index_to_xy(cluster.plume_mask(), MC).T
-             areas[id] = areas[id] + len(proj)
-           #if plume has condensed
-           if cluster.has_condensed():
+           if id in filtered_ids:
+                #only increment lifetime of plumes if there are dry plume
+                #points (i.e. the plume is has not completely formed into a cloud)
+                if has_condensed[id]:
+                    plume = cluster.plume_mask()
+                    #the condensed region is always a subset of the plume 
+                    condensed = cluster.condensed_mask()
+                    dry_plume = np.setdiff1d(plume, condensed)
+                    if len(dry_plume) > 0:
+                        lifetimes[id] = lifetimes[id] + MC['dt']
+                        proj = index_to_xy(dry_plume, MC).T
+                        areas[id] = areas[id] + len(proj)
+                else:
+                    lifetimes[id] = lifetimes[id] + MC['dt']
+                    proj = index_to_xy(cluster.plume_mask(), MC).T
+                    areas[id] = areas[id] + len(proj)
+           #label plumes that have just condensed, find height and time of condensation
+           if cluster.has_condensed() and not has_condensed[id]:
                has_condensed[id] = True
 
-    #filter out noise (i.e. clusters that exist only for a single time step)
-    plume_ids = np.where(lifetimes > dt)[0]
+    lifetimes = lifetimes[filtered_ids]
+    areas = areas[filtered_ids]
+    areas = areas*(MC['dx']*MC['dy'])/(lifetimes/MC['dt'])
+    has_condensed = has_condensed[filtered_ids]
 
-    #average the plume projection areas over their lifetime
-    areas = areas*(dx*dy)/(lt_before_condensed/dt)
-
-    lifetimes = lifetimes[plume_ids]
-    lt_before_condensed = lt_before_condensed[plume_ids]
-    areas = areas[plume_ids]
-    has_condensed = has_condensed[plume_ids]
-
-    return lt_before_condensed, areas, has_condensed
+    return lifetimes, areas, has_condensed
 
    
 
